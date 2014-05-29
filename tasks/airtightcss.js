a6672af66057c3f49e6b9ea067cb1ab9e7533f14
Nick Matantsev <nick.m@myplanetdigital.com>
2014-05-28 23:25:44 -0400

    Using updated ignore code

--- tasks/airtightcss.js
+++ tasks/airtightcss.js
@@ -14,17 +14,12 @@ module.exports = function (grunt) {
 
         this.filesSrc.forEach(function(file) {
             var css = fs.readFileSync(file, 'utf8');
-            var ignoreErrors = /\/\*\s*grunt-airtightcss-ignore-file\s*\*\//.test(css);
 
             airtightCssLint(css, function (line, column, msg) {
                 var logMessage = '[' + file + ':' + line + ':' + column + '] ' + msg;
 
-                if (!ignoreErrors) {
-                    grunt.log.error(logMessage);
-                    hasErrors = true;
-                } else {
-                    grunt.log.writeln('IGNORED ' + logMessage);
-                }
+                grunt.log.error(logMessage);
+                hasErrors = true;
             });
         });
 
